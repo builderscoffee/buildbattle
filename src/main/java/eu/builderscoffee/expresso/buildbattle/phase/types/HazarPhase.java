bbf7c907b9e0db4e11b6271500123f028936ae8c
Ketsu_sama <contactgameincraft@gmail.com>
2021-02-26 20:46:43 +0100

    final phase system

--- src/main/java/eu/builderscoffee/expresso/buildbattle/phase/types/HazarPhase.java
+++ src/main/java/eu/builderscoffee/expresso/buildbattle/phase/types/HazarPhase.java
@@ -1,33 +1,28 @@
 package eu.builderscoffee.expresso.buildbattle.phase.types;
 
 import eu.builderscoffee.expresso.Main;
-import eu.builderscoffee.expresso.buildbattle.expressos.engine.IGameEngine;
-import eu.builderscoffee.expresso.buildbattle.expressos.engine.types.HazarEngine;
+import eu.builderscoffee.expresso.buildbattle.expressos.listeners.HazarListener;
+import eu.builderscoffee.expresso.utils.Log;
+import lombok.val;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class HazarPhase extends GamePhase {
 
-    public static HazarEngine hazarEngine;
-
     public HazarPhase(int maxTime) {
         super(maxTime);
-        // Enregistrer l'engine de la partie
-        hazarEngine = new HazarEngine(Main.getInstance());
-    }
-
-    @Override
-    public String name() {
-        return "Game hazard";
+        // Enregistrer l'evenement custom
+        new HazarListener(Main.getInstance());
     }
 
     @Override
     public BukkitRunnable runnable() {
-
+        // Shuffle une liste de block
+        val runnable = new BukkitRunnable() {
+            @Override
+            public void run() {
+                Log.get().info("Test super phase runnable");
+            }
+        }.runTaskTimerAsynchronously(Main.getInstance(), 0, 20);
         return super.runnable();
-    }
-
-    @Override
-    public IGameEngine engine() {
-        return hazarEngine;
     }
 }
--- src/main/java/eu/builderscoffee/expresso/buildbattle/phase/types/JuryPhase.java
+++ src/main/java/eu/builderscoffee/expresso/buildbattle/phase/types/JuryPhase.java
@@ -1,9 +1,17 @@
 package eu.builderscoffee.expresso.buildbattle.phase.types;
 
+import eu.builderscoffee.api.utils.Title;
+import eu.builderscoffee.expresso.Main;
 import eu.builderscoffee.expresso.buildbattle.BBGameManager;
 import eu.builderscoffee.expresso.buildbattle.phase.BBPhase;
 import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+import static org.bukkit.Bukkit.getOnlinePlayers;
+import static org.bukkit.GameMode.CREATIVE;
 
 public class JuryPhase implements BBPhase {
 
@@ -33,7 +41,13 @@ public class JuryPhase implements BBPhase {
             @Override
             public void run() {
                 // Kick tous les joueurs
-                Bukkit.getOnlinePlayers().forEach(s -> s.kickPlayer("Les plots sont en cours de notation"));
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        Bukkit.getOnlinePlayers().forEach(s -> s.kickPlayer("Les plots sont en cours de notation"));
+                    }
+                }.runTask(Main.getInstance());
+
 
                 // Permettre la notation des plots
                 // TODO some stuff here
