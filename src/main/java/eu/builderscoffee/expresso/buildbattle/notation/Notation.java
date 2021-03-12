9a2d1538de42052a8d3cb916a4f23b7fede776d5
Ketsu_sama <contactgameincraft@gmail.com>
2021-03-12 21:02:55 +0100

    Feature/jury notation (#6)
    * C bon
    
    * Presque fini
    
    * Fini le system de la notation
    
    * Manque inv (sinon fini)
    
    * Final version
    
    Co-authored-by: leod1 <leod1234444@gmail.com>

---
+++ src/main/java/eu/builderscoffee/expresso/buildbattle/notation/Notation.java
@@ -0,0 +1,61 @@
+package eu.builderscoffee.expresso.buildbattle.notation;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+
+public class Notation {
+    private UUID UUIDP;
+    private int baute,crea,amenagement,folklore,fun;
+
+
+    public boolean addCrea(int crea) {
+        if ((this.crea = this.crea + crea) > 22){
+            return false;
+        } else {
+            this.crea = this.crea + crea;
+            return true;
+        }
+    }
+
+    public boolean addBaute(int baute) {
+        if ((this.baute = this.baute + baute) > 30){
+            return false;
+        } else {
+            this.baute = this.baute + baute;
+            return true;
+        }
+    }
+
+    public boolean addAmenagement(int amenagement) {
+        if ((this.amenagement = this.amenagement + amenagement) > 22){
+            return false;
+        } else {
+            this.amenagement = this.amenagement + amenagement;
+            return true;
+        }
+    }
+
+    public boolean addFolklore(int folklore) {
+        if ((this.folklore = this.folklore + folklore) > 22){
+            return false;
+        } else {
+            this.folklore = this.folklore + folklore;
+            return true;
+        }
+    }
+    public boolean addFun(int fun) {
+        if ((this.fun = this.fun + fun) > 4){
+            return false;
+        } else {
+            this.fun = this.fun + fun;
+            return true;
+        }
+    }
+
+}
