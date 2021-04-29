420bc87929fa94d51292dcc4552b8cfb7d631b47
Ketsu_sama <contactgameincraft@gmail.com>
2021-04-29 22:12:18 +0200

    EndPhase

---
+++ src/main/java/eu/builderscoffee/expresso/buildbattle/phase/types/EndPhase.java
@@ -0,0 +1,45 @@
+package eu.builderscoffee.expresso.buildbattle.phase.types;
+
+import eu.builderscoffee.expresso.Main;
+import eu.builderscoffee.expresso.buildbattle.BBGameManager;
+import eu.builderscoffee.expresso.buildbattle.expressos.engine.IGameEngine;
+import eu.builderscoffee.expresso.buildbattle.phase.BBPhase;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class EndPhase implements BBPhase {
+
+    public EndPhase() {
+        // Mettre fin à la partie
+        Main.getBbGame().getBbGameManager().endGame();
+    }
+
+    @Override
+    public String name() {
+        return "Fin de la partie";
+    }
+
+    @Override
+    public String description() {
+        return "Mettre fin à la partie";
+    }
+
+    @Override
+    public int time() {
+        return 0;
+    }
+
+    @Override
+    public BBGameManager.BBState state() {
+        return BBGameManager.BBState.ENDING;
+    }
+
+    @Override
+    public BukkitRunnable runnable() {
+        return null;
+    }
+
+    @Override
+    public IGameEngine engine() {
+        return null;
+    }
+}
