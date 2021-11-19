5fafac7b4165467b9ce02340c89d04711a35d693
Ketsu_sama <contactgameincraft@gmail.com>
2021-11-19 19:23:00 +0100

    Adding missing parts RequestConfig

--- src/main/java/eu/builderscoffee/expresso/buildbattle/config/configs/RequestConfig.java
+++ src/main/java/eu/builderscoffee/expresso/buildbattle/config/configs/RequestConfig.java
@@ -2,7 +2,15 @@ package eu.builderscoffee.expresso.buildbattle.config.configs;
 
 import eu.builderscoffee.commons.common.redisson.packets.ServerManagerRequest;
 import eu.builderscoffee.commons.common.redisson.packets.ServerManagerResponse;
+import eu.builderscoffee.expresso.ExpressoBukkit;
+import eu.builderscoffee.expresso.buildbattle.BuildBattleInstanceType;
 import eu.builderscoffee.expresso.buildbattle.config.ConfigTemplate;
+import eu.builderscoffee.expresso.buildbattle.config.configs.game.GameConfig;
+import eu.builderscoffee.expresso.buildbattle.config.configs.game.GameSettings;
+import eu.builderscoffee.expresso.buildbattle.config.configs.game.Start;
+import org.bukkit.Bukkit;
+
+import java.util.Objects;
 
 public class RequestConfig extends ConfigTemplate {
     public RequestConfig() {
@@ -12,7 +20,33 @@ public class RequestConfig extends ConfigTemplate {
     @Override
     public ServerManagerResponse request(ServerManagerRequest request, ServerManagerResponse response) {
         System.out.println(">> Request " + this.getClass().getSimpleName());
-        return redirect(BuildbattleInstanceType.class, response);
+        // Le type d'instance à déja été définis ?
+        if (Objects.isNull(ExpressoBukkit.getBbGame()) || ExpressoBukkit.getBbGame().getBbGameTypes().equals(BuildBattleInstanceType.NONE)) {
+            return redirect(BuildbattleInstanceType.class, response);
+            // L'instance définie est expresso mais le type d'expresso n'est pas défini lui
+        } else if (ExpressoBukkit.getBbGame().getBbGameTypes().equals(BuildBattleInstanceType.EXPRESSO) && Objects.isNull(ExpressoBukkit.getBbGame().getExpressoGameType())) {
+            return redirect(ExpressoType.class, response);
+            // L'instance définie est classic mais le type n'est pas défini lui
+        } else if (ExpressoBukkit.getBbGame().getBbGameTypes().equals(BuildBattleInstanceType.CLASSIC) && Objects.isNull(ExpressoBukkit.getBbGame().getClassicGameType())) {
+            return redirect(GameConfig.class, response);
+            // L'instance définie est tournois mais le type n'est pas défini lui
+        } else if (ExpressoBukkit.getBbGame().getBbGameTypes().equals(BuildBattleInstanceType.TOURNAMENT) && Objects.isNull(ExpressoBukkit.getBbGame().getTournamentGameType())) {
+            return redirect(BuildbattleInstanceType.class, response);
+        } else if (Objects.isNull(ExpressoBukkit.getBbGame().getInstancePhases())) {
+            // Le thème de la partie n'est pas définie
+            return redirect(ExpressoPlayTime.class, response);
+        } else if (Objects.isNull(ExpressoBukkit.getBbGame().getBbGameManager().getThemes())) {
+            return redirect(ExpressoTheme.class, response);
+            // La map n'est pas générée
+        } else if (Objects.isNull(Bukkit.getWorld(ExpressoBukkit.getSettings().getPlotWorldName()))) {
+            return redirect(PlotConfig.class, response);
+            // La partie n'est pas démarrer ou est en pause
+        } else if (!ExpressoBukkit.getBbGame().isReady() || ExpressoBukkit.getBbGame().isPaused()) {
+            return redirect(Start.class, response);
+            // La partie est démarrer
+        } else {
+            return redirect(GameSettings.class, response);
+        }
     }
 
     @Override
