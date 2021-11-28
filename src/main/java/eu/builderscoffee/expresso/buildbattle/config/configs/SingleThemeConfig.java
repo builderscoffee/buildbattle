27c865ae373b3a998ac7374bea225291c42d8006
Ketsu_sama <contactgameincraft@gmail.com>
2021-11-28 20:21:15 +0100

    Theme translation update

--- src/main/java/eu/builderscoffee/expresso/buildbattle/config/configs/SingleThemeConfig.java
+++ src/main/java/eu/builderscoffee/expresso/buildbattle/config/configs/SingleThemeConfig.java
@@ -1,5 +1,6 @@
 package eu.builderscoffee.expresso.buildbattle.config.configs;
 
+import eu.builderscoffee.api.bukkit.utils.ItemBuilder;
 import eu.builderscoffee.api.common.data.DataManager;
 import eu.builderscoffee.api.common.data.tables.BuildbattleThemeEntity;
 import eu.builderscoffee.commons.common.redisson.packets.ServerManagerRequest;
@@ -7,6 +8,7 @@ import eu.builderscoffee.commons.common.redisson.packets.ServerManagerResponse;
 import eu.builderscoffee.expresso.ExpressoBukkit;
 import eu.builderscoffee.expresso.buildbattle.config.ConfigTemplate;
 import lombok.val;
+import org.bukkit.Material;
 
 public class SingleThemeConfig extends ConfigTemplate {
 
@@ -29,9 +31,8 @@ public class SingleThemeConfig extends ConfigTemplate {
         // Get Themes form database
         val data = DataManager.getBuildbattleThemeStore().select(BuildbattleThemeEntity.class).get();
 
-
         // Paginate the themes
-        //data.forEach(theme -> pageItemsAction.addItem(new ItemBuilder(Material.MAP).setName("§a" + theme.getNames().stream().filter(theme -> theme.getLanguage().name.equals("FR")).findFirst().get()).build(), theme.getNames()));
+        data.forEach(theme -> pageItemsAction.addItem(new ItemBuilder(Material.MAP).setName("§a" + theme.getNames().stream().filter(translation -> translation.getLanguage().equals("FR")).findAny().get().getName()).build(), theme.getNames().stream().filter(translation -> translation.getLanguage().equals("FR")).findAny().get().getName()));
 
         // Add Action to response
         response.getActions().add(pageItemsAction);
